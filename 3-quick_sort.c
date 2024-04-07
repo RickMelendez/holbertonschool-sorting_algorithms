d58ca5a43ce8e4c182806d14588a7d0c61fd8d0f
Carlos <sotocarlos1956@gmail.com>
2024-04-07 00:36:54 -0400

    saving 2. 3 and modifications on sort.h

---
+++ 3-quick_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+#include "lomus.c"
+/**
+ * quick_sort- sorts
+ * @array- pointer to array
+ * @size: number of elements
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return; 
+
+	quick_sort_r(array, 0, size - 1);
+}
--- sort.h
+++ sort.h
@@ -9,7 +9,6 @@
 
 /**
  * struct listint_s - Doubly linked list node
- *
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
@@ -25,12 +24,13 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-int lomuto_partition(int *array, int low, int high, size_t size);
-void quick_sort_recursive(int *array, int low, int high, size_t size);
+int lomuto_partition(int *array, int low, int high);
 void quick_sort(int *array, size_t size);
+
+void swap(int *a, int *b);
+void quick_sort_r(int *array, int low, int high);
 
 #endif /* SORT_HEADER_H */
