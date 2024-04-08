5a62bf5769dced4a5558f415bc1f5cb281f37843
Carlos <sotocarlos1956@gmail.com>
2024-04-07 22:23:57 -0400

    fixing declaration error

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,12 +7,14 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
+
 	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		size_t min_index = i;
+
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_index])
@@ -23,6 +25,7 @@ void selection_sort(int *array, size_t size)
 		if (min_index != i)
 		{
 			int tmp = array[i];
+
 			array[i] = array[min_index];
 			array[min_index] = tmp;
 
