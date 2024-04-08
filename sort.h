471c8d2c8794e1ec591b797c79c2f52a6837b7af
Carlos <sotocarlos1956@gmail.com>
2024-04-07 23:53:38 -0400

    header

--- sort.h
+++ sort.h
@@ -27,10 +27,10 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-int lomuto_partition(int *array, int low, int high);
+int lomuto_partition(int *array, int low, int high, size_t size);
 void quick_sort(int *array, size_t size);
 
 void swap(int *a, int *b);
-void quick_sort_r(int *array, int low, int high);
+void quick_sort_r(int *array, int low, int high, size_t size);
 
 #endif /* SORT_HEADER_H */
